2475d266e80e0b4c8818cbd023e333cd1f647197
Andreas Kjellqvist <linco95@gmail.com>
2017-02-27 02:50:32 +0100

    just removing some whitespace

--- ADSlab/ADSlab/Insertion.cpp
+++ ADSlab/ADSlab/Insertion.cpp
@@ -9,10 +9,10 @@ InsertionSorter::~InsertionSorter(){
 std::vector<int> InsertionSorter::sort(std::vector<int> input){ // O(n ^ 2)
 
 	int sorted = 0;												// O(1)
-	for (int i = sorted + 1; i < input.size(); i++) { // O(n) * O(n) = O(n^2)
+	for (int i = sorted + 1; i < input.size(); i++) {			// O(n) * O(n) = O(n^2)
 		int index = i;											// O(1)
 		int value = input.at(index);							// O(1)
-		for (int k = sorted; k >= 0; k--) {							// O(n)
+		for (int k = sorted; k >= 0; k--) {						// O(n)
 			input.erase(input.begin() + index);					// O(1)
 			input.insert(input.begin() + index, input.at(k));	// O(1)
 			auto test = input.at(k);							// O(1)
@@ -27,10 +27,5 @@ std::vector<int> InsertionSorter::sort(std::vector<int> input){ // O(n ^ 2)
 		input.insert(input.begin() + index, value);				// O(1)
 		sorted++;												// O(1)
 	}
-
-	
-
-
-
 	return input;
 }
