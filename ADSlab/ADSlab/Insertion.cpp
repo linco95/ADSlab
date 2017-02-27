4db1a9d3aa1a5f8a8016fd64d4675baf7253e236
Sebasitan Johansson <sebbe.johansson1995@gmail.com>
2017-02-27 02:09:06 +0100

    Temi thingys complexities thkingys.

--- ADSlab/ADSlab/Insertion.cpp
+++ ADSlab/ADSlab/Insertion.cpp
@@ -6,27 +6,29 @@ InsertionSorter::InsertionSorter(){
 InsertionSorter::~InsertionSorter(){
 }
 
-std::vector<int> InsertionSorter::sort(std::vector<int> input){
+std::vector<int> InsertionSorter::sort(std::vector<int> input){ // O(n ^ 2)
 
-	int sorted = 0;
-	for (int i = sorted + 1; i < input.size(); i++) {
-		int index = i;
-		int value = input.at(index);
-		for (int k = sorted; k >= 0; k--) {
-			input.erase(input.begin() + index);
-			input.insert(input.begin() + index, input.at(k));
-			auto test = input.at(k);
-			if (test > value) {
-				index--;
+	int sorted = 0;												// O(1)
+	for (int i = sorted + 1; i < input.size(); i++) { // O(n) * O(n) = O(n^2)
+		int index = i;											// O(1)
+		int value = input.at(index);							// O(1)
+		for (int k = sorted; k >= 0; k--) {							// O(n)
+			input.erase(input.begin() + index);					// O(1)
+			input.insert(input.begin() + index, input.at(k));	// O(1)
+			auto test = input.at(k);							// O(1)
+			if (test > value) {									// O(1)
+				index--;										// O(1)
 			}
-			else {
-				break;
+			else {												// O(1)
+				break;											// O(1)
 			}
 		}
-		input.erase(input.begin() + index);
-		input.insert(input.begin() + index, value);
-		sorted++;
+		input.erase(input.begin() + index);						// O(1)
+		input.insert(input.begin() + index, value);				// O(1)
+		sorted++;												// O(1)
 	}
+
+	
 
 
 
